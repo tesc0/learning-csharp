44ae9772d25bb458211cd9b09c64b4a94aad3040
tesc0 <zs0lt.santa@gmail.com>
2017-07-07 23:32:31 +0200

    basic mp3 player

---
+++ mp3/mp3/MainForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Zsolt
+ * Date: 2017.07.04.
+ * Time: 23:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace mp3
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button lejatszas;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.lejatszas = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.megallitas = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lejatszas
+            // 
+            this.lejatszas.Location = new System.Drawing.Point(169, 13);
+            this.lejatszas.Name = "lejatszas";
+            this.lejatszas.Size = new System.Drawing.Size(35, 23);
+            this.lejatszas.TabIndex = 0;
+            this.lejatszas.Text = "play";
+            this.lejatszas.UseVisualStyleBackColor = true;
+            this.lejatszas.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 42);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(355, 22);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // megallitas
+            // 
+            this.megallitas.Location = new System.Drawing.Point(169, 13);
+            this.megallitas.Name = "megallitas";
+            this.megallitas.Size = new System.Drawing.Size(35, 23);
+            this.megallitas.TabIndex = 4;
+            this.megallitas.Text = "stop";
+            this.megallitas.UseVisualStyleBackColor = true;
+            this.megallitas.Visible = false;
+            this.megallitas.Click += new System.EventHandler(this.megallitas_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(272, 13);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(26, 23);
+            this.plus.TabIndex = 8;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(77, 12);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(26, 23);
+            this.minus.TabIndex = 9;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(373, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(395, 95);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 122);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.megallitas);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lejatszas);
+            this.Name = "MainForm";
+            this.Text = "mp3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button megallitas;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ mp3/mp3/MainForm.cs
@@ -0,0 +1,229 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Zsolt
+ * Date: 2017.07.04.
+ * Time: 23:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using WMPLib;
+
+namespace mp3
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+
+        WindowsMediaPlayer wplayer = new WindowsMediaPlayer();
+
+        public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			
+            //check wmp version
+			RegistryKey myKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\MediaPlayer\\PlayerUpgrade", false);
+			string version = (string) myKey.GetValue("PlayerVersion");
+			
+			version = version.Substring(0, 2);
+			int versionNumber = Int32.Parse(version);
+			if(versionNumber < 10) {
+				lejatszas.Visible = false;
+			}
+
+            //listview drag&drop
+            listView1.AllowDrop = true;
+            listView1.DragDrop += listViewFiles_DragDrop;
+            listView1.DragEnter += listViewFiles_DragEnter;
+        }
+		void Button1Click(object sender, EventArgs e)
+		{
+            wplayer.PlayStateChange +=
+                new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(Player_PlayStateChange);
+            wplayer.MediaError +=
+                new WMPLib._WMPOCXEvents_MediaErrorEventHandler(Player_MediaError);
+
+            //create a playlist
+            wplayer.currentPlaylist = wplayer.newPlaylist("playlist", "");            
+            foreach (ListViewItem itemRow in listView1.Items)
+            {
+                for (int i = 0; i < itemRow.SubItems.Count; i++)
+                {
+                    string song_path = itemRow.SubItems[i].Text;
+                    //Console.WriteLine(itemRow.SubItems[i].Text);
+                    wplayer.currentPlaylist.appendItem(wplayer.newMedia(song_path));
+                }
+            }
+
+            //load the file
+            //wplayer.URL = "e:\\Famous Last Words - Council Of The Dead (2014) 320\\02.Council of the Dead.mp3";
+            //base volume
+            wplayer.settings.volume = 50;
+            //let it rock
+            wplayer.controls.play();
+            
+		}
+
+        private void megallitas_Click(object sender, EventArgs e)
+        {
+            //stop
+            wplayer.controls.stop();
+        }
+
+        /*
+            0	Undefined	Windows Media Player is in an undefined state.
+            1	Stopped	Playback of the current media item is stopped.
+            2	Paused	Playback of the current media item is paused. When a media item is paused, resuming playback begins from the same location.
+            3	Playing	The current media item is playing.
+            4	ScanForward	The current media item is fast forwarding.
+            5	ScanReverse	The current media item is fast rewinding.
+            6	Buffering	The current media item is getting additional data from the server.
+            7	Waiting	Connection is established, but the server is not sending data. Waiting for session to begin.
+            8	MediaEnded	Media item has completed playback.
+            9	Transitioning	Preparing new media item.
+            10	Ready	Ready to begin playing.
+            11	Reconnecting	Reconnecting to stream.
+             
+             */
+
+        private void Player_PlayStateChange(int NewState)
+        {
+            if ((WMPPlayState)NewState == WMPPlayState.wmppsPlaying)
+            {
+                //if started, the button is replaced with 'stop'
+                lejatszas.Visible = false;
+                megallitas.Visible = true;
+
+                //progressbar
+                double dur = wplayer.currentMedia.duration;
+                progressBar1.Maximum = (int)dur;
+
+                //duration
+                label2.Text = wplayer.currentMedia.durationString;
+                
+                //artist
+                label3.Text = "Előadó: " + wplayer.currentMedia.getItemInfo("Artist");
+
+                //song
+                label4.Text = "Szám / Album: " + wplayer.currentMedia.getItemInfo("Title") + " (" + wplayer.currentMedia.getItemInfo("Album") + ")";
+
+                //highlight
+                foreach (ListViewItem itemRow in listView1.Items)
+                {
+                    for (int i = 0; i < itemRow.SubItems.Count; i++)
+                    {
+                        string song_path = itemRow.SubItems[i].Text;
+                        //Console.WriteLine(itemRow.SubItems[i].Text);
+                        if (song_path == wplayer.currentMedia.getItemInfo("SourceURL"))
+                        {
+                            itemRow.SubItems[i].BackColor = Color.Red;
+                        }
+                    }
+                }
+
+            } else if ((WMPPlayState)NewState == WMPPlayState.wmppsStopped)
+            {
+                //close
+                this.Close();
+            } else
+            {
+
+            }
+
+        }
+
+        private void Player_MediaError(object pMediaObject)
+        {
+            //error messages
+            MessageBox.Show("Cannot play media file.");
+            this.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //current position of the song
+            //update time and progressbar both
+            label1.Text = wplayer.controls.currentPositionString;
+            progressBar1.Value = (int)wplayer.controls.currentPosition;
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+
+            //+volume
+            wplayer.settings.volume++;
+            if (wplayer.settings.volume == 100)
+            {
+                plus.Enabled = false;
+            } 
+            if(minus.Enabled == false)
+            {
+                minus.Enabled = true;
+            }
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+
+            //-volume
+            wplayer.settings.volume--;
+            if(wplayer.settings.volume == 0)
+            {
+                minus.Enabled = false;
+            }
+            if (plus.Enabled == false)
+            {
+                plus.Enabled = true;
+            }
+        }
+
+        private void listViewFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listViewFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+
+                if (System.IO.Path.GetExtension(file).Equals(".mp3", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    //add the file
+                    listView1.Items.Add(file);
+                }
+                else
+                {
+                    MessageBox.Show("Must be an mp3!");
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //next
+            wplayer.controls.next();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //previous
+            wplayer.controls.previous();
+        }
+    }
+}
